c48c9d60725e32921cfbcd703b3ead320170ce85
Abdel Rahman Elleithy <abdelElleithy@gmail.com>
2021-11-19 03:25:34 -0500

    Added play button ui

--- ReplitCodingApp/Colors.swift
+++ ReplitCodingApp/Colors.swift
@@ -11,4 +11,7 @@ import SwiftUI
 extension Color {
     static var replitBackgroundColor = Color(red: 25/255, green: 32/255, blue: 44/255)
     static var lineNumbersGrayColor = Color(red: 122/255, green: 122/255, blue: 122/255)
+    
+    static var playButtonDarkGreen = Color(red: 0/255, green: 65/255, blue: 23/255)
+    static var playButtonBrightGreen = Color(red: 0/255, green: 134/255, blue: 38/255)
 }
--- ReplitCodingApp/MainView.swift
+++ ReplitCodingApp/MainView.swift
@@ -11,18 +11,9 @@ struct MainView: View {
     
     @State private var codeText = ""
     @State private var lineNumbersText = "1"
-//    @State private var codingLines = [Int]()
     
     var body: some View {
-        HStack {
-//            LineNumbersTextView(text: $lineNumbersText, contentOffset: $scrollingContentOffset)
-//                .padding(.vertical)
-////                .padding(.trailing, 2)
-//                .font(.body)
-//                .frame(maxWidth: 35)
-//
-//            Divider()
-            
+        ZStack {
             CodingTextView(text: $codeText, isSelectable: true)
                 .padding(.vertical)
                 .onAppear {
@@ -35,24 +26,25 @@ struct MainView: View {
                         }
                     }
                 }
-//                .onChange(of: codingLines) { newValue in
-//                    var currentNumber = 1
-//                    var lineNumbers = ""
-//
-//                    print(newValue)
-//                    for numberOfLines in newValue {
-//
-//                        lineNumbers = "\(lineNumbers)\(currentNumber)\(String(repeating: "\n", count: numberOfLines))"
-//                        print(currentNumber)
-//                        currentNumber += 1
-//                    }
-//
-//                    lineNumbersText = lineNumbers
-//                }
-            
-            
+            HStack {
+                Spacer()
+                
+                VStack {
+                    Spacer()
+                    
+                    Button {
+                        print("Yo")
+                    } label: {
+                        Image(systemName: "play.fill")
+                            .foregroundColor(.white)
+                            .frame(width: 70, height: 70)
+                            .background(Color.playButtonBrightGreen)
+                            .clipShape(Circle())
+                            .padding()
+                    }
+                }
+            }
         }
-        
     }
 }
 
